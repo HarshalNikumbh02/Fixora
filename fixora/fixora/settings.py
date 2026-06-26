d5b10c0022764a3fd851c3a9c40fe5670740d7a5
HarshalNikumbh02 <hnikumbh12@gmail.com>
2026-06-26 11:49:35 +0530

    Updated Allowed Hosts and disabled DEBUG for Production

--- fixora/fixora/settings.py
+++ fixora/fixora/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-z@q#ryr$3+rr2h5+x1ckuink_)(mk$r#kc%v_&ccd33&)9)lh_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://fixora-app.onrender.com' , '127.0.0.1', 'localhost']
 
 
 # Application definition
